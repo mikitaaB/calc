ec4e599e6a650255ecb77ae4174e23e524abb9e8
Mikita Buinavets <mikita.buinavets@arua.co>
2023-09-30 15:10:43 +0300

    Remove unused empty tag

--- src/components/EditScheduleDialog/EditScheduleHeader.tsx
+++ src/components/EditScheduleDialog/EditScheduleHeader.tsx
@@ -6,18 +6,16 @@ export const EditScheduleHeader = memo(function ({
 	handleCloseDialog,
 }: EditScheduleHeaderPropsType) {
 	return (
-		<>
-			<div className="modal-header">
-				<h1 className="modal-title fs-5" id="editScheduleDialogLabel">
-					{headerText}
-				</h1>
-				<button
-					type="button"
-					className="btn-close"
-					aria-label="Close"
-					onClick={handleCloseDialog}
-				></button>
-			</div>
-		</>
+		<div className="modal-header">
+			<h1 className="modal-title fs-5" id="editScheduleDialogLabel">
+				{headerText}
+			</h1>
+			<button
+				type="button"
+				className="btn-close"
+				aria-label="Close"
+				onClick={handleCloseDialog}
+			></button>
+		</div>
 	);
 });
